35d7b97eb9398f04176f3b544934453becc8f1a0
orifjohn <orifjondev@gmail.com>
2022-07-16 16:21:25 +0500

    feat: post model

---
+++ post/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Author(models.Model):
+    pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField(max_length=50)
+    content = models.TextField(max_length=500)
+    image = models.ImageField()
+    time_read = models.IntegerField()
+    status = models.BooleanField(default=False)
